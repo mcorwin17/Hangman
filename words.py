aa4c90fd2527a10f4a0c07ad94731e4d03cfc876
Maxwell Corwin <maxwellcorwin@Maxwells-MacBook-Pro.local>
2025-05-26 15:27:53 -0400

    Initial commit of Hangman game

---
+++ words.py
@@ -0,0 +1,9 @@
+words = [
+    "organic", "explain", "against", "decline", "caution", "comfort", "concept", "backing", "advisor", "chapter",
+    "ability", "absence", "holiday", "machine", "monitor", "virtual", "fiction", "protect", "ancient", "density",
+    "private", "science", "crystal", "default", "village", "involve", "airport", "outcome", "another", "justice",
+    "content", "anxiety", "variety", "notable", "clothes", "restore", "romance", "attempt", "summary", "context",
+    "feature", "cabinet", "despite", "extract", "storage", "fitness", "radical", "teacher", "believe", "revenue",
+    "refugee", "natural", "upgrade", "careful", "account", "digital", "various", "benefit", "various", "village",
+    "ability", "towards", "dispute", "project", "towards", "restore", "dispute", "release", "freedom", "release"
+]
